54735d6f2367ac14eab0a89d76b4a6633160c452
ajweb <info@ajweb.es>
2011-06-20 13:05:21 +0200

    removing files as part of filesystem reorganization

--- init_csvProcess_checkRowCount.rb
+++
@@ -1,9 +0,0 @@
-require 'csvProcess.rb'
-
-tablaProductos = ProcessCSV.new('/Users/angeljusto/Desktop/insercion_osculati_products_table_OK_con_images.csv', '|')
-
-
-tablaProductos.newCSVArray.each do |row|
-	if !row.length==42 then puts row[3] end
-end
-	
